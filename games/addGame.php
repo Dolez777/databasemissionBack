b6a4f931704e683f042ef799071f166f22015c9a
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-05-08 17:55:51 +0300

    addgame

--- games/addGame.php
+++ games/addGame.php
@@ -3,7 +3,5 @@ require_once INC_DIR."functions.php";
 require_once INC_DIR."headers.php";
 require_once MODULES_DIR."games.php";
 
-$input = json_decode(file_get_contents("php://input"));
-$name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    getgames();
 
-addGame($name);
