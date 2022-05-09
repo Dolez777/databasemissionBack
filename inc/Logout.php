c7c7d88e94e8c641a617f752b6d4b02acbfe6825
Jussi Wedman <juswedma@gmail.com>
2022-05-09 17:09:15 +0300

    turha login poistettu

--- inc/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   
-    if(isset($_SESSION["username"])){
-        logout();
-    }else{
-        echo '<div class="alert alert-success" role="alert"> Olet kirjautunut ulos </div>';
-    }
-
-?>
